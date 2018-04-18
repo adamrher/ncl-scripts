22938af8fc211732c8c2a5acb4fd21db70a81826
Adam Herrington <aherring@cheyenne3.ib0.cheyenne.ucar.edu>
2018-04-18 15:27:33 -0600

    dzonal script

---
+++ diff2case.sh
@@ -0,0 +1,33 @@
+#!/bin/tcsh
+# compute omega diagnostics
+
+if ( "$#argv" != 4) then
+  echo "Wrong number of arguments specified:"
+  echo "  -arg 1 case1 string"
+  echo "  -arg 2 is fincl number (e.g. h0)"
+  echo "  -arg 3 case2 string"
+  echo "  -arg 4 is fincl number (e.g. h0)"
+  exit
+endif
+set n = 1
+set case1 = "$argv[$n]"
+set n = 2
+set fincl1 = "$argv[$n]"
+set n = 3
+set case2 = "$argv[$n]"
+set n = 4
+set fincl2 = "$argv[$n]"
+if (`hostname` == "hobart.cgd.ucar.edu") then
+  set data_dir = "/scratch/cluster/$USER/"
+  set ncl_dir = "/home/$USER/2017-physres/ncl-scripts"
+  echo "You are on Hobart"
+  echo "NCL directory is "$ncl_dir
+else
+  set data_dir = "/glade2/scratch2/$USER/"
+  set ncl_dir = "/glade/p/work/$USER/CESM2/git-ncl-scripts"
+  echo "You are on Glade"
+  echo "NCL directory is "$ncl_dir
+endif
+
+ncl 'dir="'$data_dir'"' 'fname1="'$case1'"' 'fname2="'$case2'"' 'fincl1="'$fincl1'"' 'fincl2="'$fincl2'"' $ncl_dir/dzonal_hgt_diff2case.ncl
+
