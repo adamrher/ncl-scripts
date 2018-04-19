5f049b2fc383526b1b41d0ed76921bf41ba70655
Adam Herrington <aherring@cheyenne2.ib0.cheyenne.ucar.edu>
2018-04-19 10:46:01 -0600

    mods

--- glob-diags.sh
+++ glob-diags.sh
@@ -19,7 +19,7 @@ if (`hostname` == "hobart.cgd.ucar.edu") then
   echo "NCL directory is "$ncl_dir
 else
   set data_dir = "/glade2/scratch2/$USER/"
-  set ncl_dir = "/glade/u/home/$USER/2017-physres/ncl-scripts/ncl/"
+  set ncl_dir = "/glade/p/work/$USER/CESM2/git-ncl-scripts/ncl/"
   echo "You are on Glade"
   echo "NCL directory is "$ncl_dir
 endif
